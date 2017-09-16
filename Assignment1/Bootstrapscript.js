3e8480de52e592e7aff5801b6c801bc61e62f7c2
Rahul <rahulmore1991@gmail.com>
2017-09-16 17:52:18 +0530

    Angular Code

---
+++ Assignment1/Bootstrapscript.js
@@ -0,0 +1,15 @@
+
+var userData = JSON.parse(localStorage.getItem('userinfo'));
+function login(){
+    var uname = document.form.username.value;
+    var password = document.form.password.value;
+    if( password == userData.password && uname == userData.username){
+        //window.path('file:///home/rahul/MMM/Login/welcome.html');
+        window.location.href = 'Bootstrapwelcome.html';
+        return false;
+        //window.location.assign('file:///home/rahul/MMM/Login/welcome.html');
+    }else{
+        alert("Please enter valid username and password"); 
+    }
+};
+
