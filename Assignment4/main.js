04c8e0ac12c24cf25b0c7959cf37b71fed766e47
Rahul <rahulmore1991@gmail.com>
2017-10-14 17:58:23 +0530

    To make repository in sync with local

---
+++ Assignment4/main.js
@@ -0,0 +1,17 @@
+app.controller('MyController', function($scope) {
+  $scope.product = [{
+  	prodId:101,
+    prodName: "abc",
+    ProdPrize: 120
+  };
+  {
+  	prodId:102,
+    prodName: "pqr",
+    ProdPrize: 120
+  };
+  {
+  	prodId:101,
+    prodName: "xyz",
+    ProdPrize: 120
+  }]
+});
